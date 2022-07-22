bbf3e350cbc13eeedde679c5f9dc017dd95336e6
ThomasCza <czarnecki.thomas@orange.fr>
2022-07-22 17:08:16 +0200

    Ajout de données
    DbController

--- src/Controller/DbTestController.php
+++ src/Controller/DbTestController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Auteur;
+use App\Entity\Emprunt;
+use App\Entity\Emprunteur;
+use App\Entity\Genre;
+use App\Entity\Livre;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +17,46 @@ class DbTestController extends AbstractController
     #[Route('/db/test', name: 'app_db_test')]
     public function index(): Response
     {
-        return $this->render('db_test/index.html.twig', [
-            'controller_name' => 'DbTestController',
-        ]);
+        // récupération du repository des auteurs
+        $repository = $doctrine->getRepository(Auteur::class);
+        // récupération de la liste complète de toutes les aurteurs
+        $auteurs = $repository->findAll();
+        // inspection de la liste
+        dump($auteurs);
+
+        // récupération du repository des emprunts
+        $repository = $doctrine->getRepository(Emprunt::class);
+        // récupération de la liste complète de toutes les emprunts
+        $emprunts = $repository->findAll();
+        // inspection de la liste
+        dump($emprunts);
+
+        // récupération du repository des emprunteurs
+        $repository = $doctrine->getRepository(Emprunteur::class);
+        // récupération de la liste complète de toutes les emprunteurs
+        $emprunteurs = $repository->findAll();
+        // inspection de la liste
+        dump($emprunteurs);
+
+        // récupération du repository des genres
+        $repository = $doctrine->getRepository(Genre::class);
+        // récupération de la liste complète de toutes les genres
+        $genres = $repository->findAll();
+        // inspection de la liste
+        dump($genres);
+
+        // récupération du repository des livres
+        $repository = $doctrine->getRepository(Livre::class);
+        // récupération de la liste complète de toutes les livres
+        $livres = $repository->findAll();
+        // inspection de la liste
+        dump($livres);
+
+        // récupération du repository des users
+        $repository = $doctrine->getRepository(User::class);
+        // récupération de la liste complète de toutes les users
+        $users = $repository->findAll();
+        // inspection de la liste
+        dump($users);
     }
 }
